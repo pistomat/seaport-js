24ee841cfae1768dae0acd550b8759e5396026b9
pistomat <pistomat@users.noreply.github.com>
2023-06-27 15:33:52 +0200

    reexport some utils

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,51 @@
 import { Seaport } from "./seaport";
+import type {
+  BalancesAndApprovals,
+  InsufficientBalances,
+  InsufficientApprovals,
+} from "./utils/balanceAndApprovalCheck";
+import {
+  getBalancesAndApprovals,
+  getInsufficientBalanceAndApprovalAmounts,
+  validateOfferBalancesAndApprovals,
+  validateBasicFulfillBalancesAndApprovals,
+  validateStandardFulfillBalancesAndApprovals,
+} from "./utils/balanceAndApprovalCheck";
+import type { TimeBasedItemParams } from "./utils/item";
+import {
+  isCurrencyItem,
+  isNativeCurrencyItem,
+  isErc20Item,
+  isErc721Item,
+  isErc1155Item,
+  isCriteriaItem,
+  getPresentItemAmount,
+  getSummedTokenAndIdentifierAmounts,
+  getMaximumSizeForOrder,
+} from "./utils/item";
 
 export { Seaport };
+export type {
+  BalancesAndApprovals,
+  InsufficientBalances,
+  InsufficientApprovals,
+};
+export {
+  getBalancesAndApprovals,
+  getInsufficientBalanceAndApprovalAmounts,
+  validateOfferBalancesAndApprovals,
+  validateBasicFulfillBalancesAndApprovals,
+  validateStandardFulfillBalancesAndApprovals,
+};
+export type { TimeBasedItemParams };
+export {
+  isCurrencyItem,
+  isNativeCurrencyItem,
+  isErc20Item,
+  isErc721Item,
+  isErc1155Item,
+  isCriteriaItem,
+  getPresentItemAmount,
+  getSummedTokenAndIdentifierAmounts,
+  getMaximumSizeForOrder,
+};
